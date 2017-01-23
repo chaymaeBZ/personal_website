7ea2ecfa1397d4b387247a1d035a4ad8a6372393
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-01-23 12:43:25 +0000

    templates added to assets pipeline

---
+++ vendor/assets/custom/custom.js
@@ -0,0 +1,7 @@
+//= require custom/js/jquery.dropotron.min.js
+//= require custom/js/jquery.min.js
+//= require custom/js/jquery.scrollgress.min.js
+//= require custom/js/jquery.scrolly.min.js
+//= require custom/js/main.js
+//= require custom/js/skel.min.js
+//= require custom/js/util.js
