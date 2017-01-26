101c98a5bd50aa0be91a001462cff15512a3dbff
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-01-26 14:28:17 +0000

    test mail form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  def send
+    c = ContactForm.new(:name => 'José', :email => 'jose@email.com', :message => 'Cool!')
+    c.deliver
+    render 'sendme'
+  end
 end
