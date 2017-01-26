ad37b739017c7b79190462bd2d4d6fbab479a80a
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-01-26 22:28:17 +0000

    bug fixes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,6 @@
 class ContactMailer < ApplicationMailer
-  def contact_email(@contact)
-    mail(to: "bazzaoui.chaymae@gmail.com", subject: @contact.subject)
+  def contact_email(contact)
+    @contact = contact
+    mail(to: "bazzaoui.chaymae@gmail.com", subject: contact.subject)
   end
 end
